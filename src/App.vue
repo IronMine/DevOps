508d82e59cef45c6ababc8c7ba720edf1067d5bf
Tristan Perrin <ironminegg@gmail.com>
2023-02-21 10:18:04 +0100

    Modification taillle de la div pricipale

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,71 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { toRefs, watch } from "vue";
+import { useQueryProvider } from "vue-query";
+import "animate.css";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import DetailsModal from "./components/DetailsModal.vue";
+import { state } from "./store";
+
+export default {
+  components: {
+    Header,
+    Footer,
+    DetailsModal,
+  },
+  setup() {
+    useQueryProvider({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          refetchOnMount: false,
+        },
+      },
+    });
+
+    watch(
+      () => state.isModalActive,
+      () => {
+        setTimeout(function () {
+          window.scrollTo({
+            left: 0,
+            top: state.scrollTop,
+          });
+        }, 0);
+      }
+    );
+    return {
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/Sign-in">Sign-in</RouterLink>
-        <RouterLink to="/Netflix">Netflix</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Header />
+  <div
+    class="min-h-screen"
+    :style="[isModalActive && `margin-top: -${scrollTop}px`]"
+  >
+    <router-view v-slot="{ Component, route }">
+      <transition
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeOut"
+        mode="out-in"
+      >
+        <component
+          :is="Component"
+          :class="[isModalActive && 'fixed']"
+          :key="route.fullPath"
+        />
+      </transition>
+    </router-view>
+  </div>
+  <transition
+    enter-active-class="animate__animated animate__fadeIn"
+    leave-active-class="animate__animated animate__fadeOut"
+  >
+    <DetailsModal v-if="isModalActive" />
+  </transition>
+  <Footer />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
